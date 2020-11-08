fecf2ce263fa32abae02d0059533d972ee50dad4
Sergio Faya <sergiofayafernandez@gmail.com>
2020-11-08 22:12:46 +0100

    master: created endpoint for qualityListing

--- src/main/java/com/idealista/infrastructure/api/AdsController.java
+++ src/main/java/com/idealista/infrastructure/api/AdsController.java
@@ -1,17 +1,36 @@
 package com.idealista.infrastructure.api;
 
+import java.util.ArrayList;
 import java.util.List;
 
+
+import com.idealista.application.model.vo.QualityAdVo;
+import com.idealista.application.service.AdService;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class AdsController {
+@RequestMapping(path = "/ads")
+public class AdsController implements InitializingBean {
 
-    //TODO añade url del endpoint
-    public ResponseEntity<List<QualityAd>> qualityListing() {
-        //TODO rellena el cuerpo del método
-        return ResponseEntity.notFound().build();
+    @Autowired
+    private AdService adService;
+
+    @GetMapping(value = "/quality")
+    public ResponseEntity<List<QualityAdVo>> qualityListing() {
+        try {
+            List<QualityAdVo> ads = new ArrayList<>();
+            ads.addAll(adService.findAllQualityAds());
+            return ResponseEntity.accepted().body(ads);
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     //TODO añade url del endpoint
@@ -24,5 +43,10 @@ public class AdsController {
     public ResponseEntity<Void> calculateScore() {
         //TODO rellena el cuerpo del método
         return ResponseEntity.notFound().build();
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        Assert.notNull(adService, "qualityAdService must be injected");
     }
 }
